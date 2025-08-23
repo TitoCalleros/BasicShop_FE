04301d84563545313376bf23fda177f5a12d0397
Tito Calleros <tito.calleros@gmail.com>
2025-08-23 01:23:09 -0600

    Add Product and ProductResponse interfaces

---
+++ basic-shop-fe/src/app/products/interfaces/product.interface.ts
@@ -0,0 +1,16 @@
+export interface ProductsResponse {
+  items:      Product[];
+  page:       number;
+  pageSize:   number;
+  totalCount: number;
+  totalPages: number;
+}
+
+export interface Product {
+  id:          string;
+  name:        string;
+  description: string;
+  price:       number;
+  stock:       number;
+}
+
