03a0902bdfa345e857a888561bc01dbbd34d5312
Tito Calleros <tito.calleros@gmail.com>
2025-08-24 14:19:59 -0600

    enforce single selection via checkbox synced with selectedId

--- basic-shop-fe/src/app/products/components/product-table/product-table.component.ts
+++ basic-shop-fe/src/app/products/components/product-table/product-table.component.ts
@@ -1,8 +1,9 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'product-table',
@@ -11,4 +12,20 @@ import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 })
 export class ProductTableComponent {
   products = input.required<Product[]>();
+
+  productService = inject(ProductsService);
+
+  changeSelectedProduct(id: string) {
+    console.log(`Selected id: ${this.productService.selectedId()} - id: ${id}`);
+
+    if (this.productService.selectedId() == id)
+    {
+      this.productService.selectedId.set(null);
+      return;
+    }
+
+    this.productService.selectedId.set(id);
+
+  }
+
 }
