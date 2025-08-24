3989fe0eb0466cfe8a28a20b4c04cb81f4068280
Tito Calleros <tito.calleros@gmail.com>
2025-08-24 01:51:32 -0600

    Import RouterLink

--- basic-shop-fe/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ basic-shop-fe/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationService } from '@shared/components/pagination/pagination.service';
@@ -7,7 +8,7 @@ import { ProductTableComponent } from "@products/components/product-table/produc
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [PaginationComponent, ProductTableComponent],
+  imports: [PaginationComponent, ProductTableComponent, RouterLink],
   templateUrl: './products-admin-page.component.html',
 })
 export class ProductsAdminPageComponent {
@@ -18,7 +19,7 @@ export class ProductsAdminPageComponent {
   productsPerPage = signal(9);
 
   productsResource = rxResource({
-    request: () => ({ page: this.paginationService.currentPage() , pageSize: this.productsPerPage() }),
+    request: () => ({ page: this.paginationService.currentPage(), pageSize: this.productsPerPage() }),
     loader: ({request}) => {
       return this.productsService.getProducts({page: request.page, pageSize: request.pageSize, search: '', sort: 0, gender: ''});
     }
