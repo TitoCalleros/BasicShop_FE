cb2f0366ed8dce79f35e5d104a50c221c3fb1cf7
Tito Calleros <tito.calleros@gmail.com>
2025-08-23 23:30:09 -0600

    Add pagination in home-page component

--- basic-shop-fe/src/app/store-front/pages/home-page/home-page.component.ts
+++ basic-shop-fe/src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,20 +2,23 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop'
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { PaginationComponent } from "src/app/shared/components/pagination/pagination.component";
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    request: () => ({}),
+    request: () => ({page: this.paginationService.currentPage()}),
     loader: ({ request }) => {
-      return this.productsService.getProducts({page: 1, pageSize: 6, search: '', sort: 0, gender: ''});
+      return this.productsService.getProducts({page: request.page, pageSize: 6, search: '', sort: 0, gender: ''});
     }
   });
 }
