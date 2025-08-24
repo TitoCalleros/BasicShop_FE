06b8eb13aa60447c5748f8e75761cea21447f057
Tito Calleros <tito.calleros@gmail.com>
2025-08-23 21:19:18 -0600

    Define product-page to show information about the product

--- basic-shop-fe/src/app/store-front/pages/product-page/product-page.component.ts
+++ basic-shop-fe/src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+import { ProductsService } from '@products/services/products.service';
+import { ProductCardResponsiveComponent } from "@products/components/product-card-responsive/product-card-responsive.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductImagePipe, CurrencyPipe, ProductCardResponsiveComponent],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+
+  productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  id: string = this.activatedRoute.snapshot.params['id'];
+
+  productResource = rxResource({
+    request: () => ({id: this.id}),
+    loader: ({request}) => {
+      return this.productsService.getProductById(request.id);
+    }
+  });
+
+}
