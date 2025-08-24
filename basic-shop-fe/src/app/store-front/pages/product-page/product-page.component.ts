1261a2dcbb0c8a71cc603b59a3acf8a667d12115
Tito Calleros <tito.calleros@gmail.com>
2025-08-23 21:35:05 -0600

    Remove unused code and add a button to return to home-page

--- basic-shop-fe/src/app/store-front/pages/product-page/product-page.component.ts
+++ basic-shop-fe/src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,14 +1,13 @@
 import { CurrencyPipe, DecimalPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 import { ProductsService } from '@products/services/products.service';
-import { ProductCardResponsiveComponent } from "@products/components/product-card-responsive/product-card-responsive.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductImagePipe, CurrencyPipe, ProductCardResponsiveComponent],
+  imports: [ProductImagePipe, CurrencyPipe, RouterLink],
   templateUrl: './product-page.component.html',
 })
 export class ProductPageComponent {
