6886c71e3445f7e0fd7516e96526aae47544dffb
Tito Calleros <tito.calleros@gmail.com>
2025-08-24 01:00:35 -0600

    Add products-admin-page component to show product list with links to product-admin-page

---
+++ basic-shop-fe/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { ProductTableComponent } from "@products/components/product-table/product-table.component";
+
+@Component({
+  selector: 'app-products-admin-page',
+  imports: [PaginationComponent, ProductTableComponent],
+  templateUrl: './products-admin-page.component.html',
+})
+export class ProductsAdminPageComponent {
+  productsService = inject(ProductsService);
+
+  paginationService = inject(PaginationService);
+
+  productsPerPage = signal(9);
+
+  productsResource = rxResource({
+    request: () => ({ page: this.paginationService.currentPage() , pageSize: this.productsPerPage() }),
+    loader: ({request}) => {
+      return this.productsService.getProducts({page: request.page, pageSize: request.pageSize, search: '', sort: 0, gender: ''});
+    }
+  });
+
+
+}
