a28bdbfad72e20721853aabd998a75d496e5d1c5
Tito Calleros <tito.calleros@gmail.com>
2025-08-24 00:53:13 -0600

    Add pagination component to gender-page

--- basic-shop-fe/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ basic-shop-fe/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -5,15 +5,18 @@ import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent, I18nSelectPipe],
+  imports: [ProductCardComponent, I18nSelectPipe, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   gender = toSignal(
     this.route.params.pipe(
@@ -26,12 +29,11 @@ export class GenderPageComponent {
     women: "para mujeres",
   };
 
-  productResource = rxResource({
+  productsResource = rxResource({
 
-    // request: () => ({gender: this.gender(), page: this.paginationService.currentPage() - 1}),
-    request: () => ({gender: this.gender()}),
+    request: () => ({gender: this.gender(), page: this.paginationService.currentPage()}),
     loader: ({request}) => {
-      return this.productsService.getProducts({page: 1, pageSize: 6, search: '', sort: 0, gender: request.gender});
+      return this.productsService.getProducts({page: request.page, pageSize: 6, search: '', sort: 0, gender: request.gender});
     }
   });
 
